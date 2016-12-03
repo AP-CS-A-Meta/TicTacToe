2e3a83562ae174a003552238fe3b30cf10d566ef
Douglas Urner <dlu@canishe.com>
2016-12-02 21:49:08 -0800

    Add wins and initWins() in preparation for detecting winner.

--- src/Board.java
+++ src/Board.java
@@ -14,6 +14,7 @@ public class Board {
 
     private JButton spaces[];
     private static int turn = 0;
+    private static int[][][] wins;
 
     Board(String title, int width, int height) {
         JFrame jf = new JFrame(title);
@@ -31,9 +32,12 @@ public class Board {
 
         jf.setSize(width * 100, height * 100);
         jf.setVisible(true);
+
+        initWins(width, height);
     }
 
     private static class TTTButton extends JButton implements ActionListener {
+
         boolean win = false;    // Not yet.
 
         public TTTButton() {
@@ -52,5 +56,64 @@ public class Board {
             }
             turn++;
         }
+    }
+
+    private static void initWins(int cols, int rows) {
+        /*
+         * Winning patterns are horizontal rows, vertical columns, and the two diagonals.
+         */
+        int patterns = cols + rows + 2;
+        wins = new int[patterns][cols][rows];
+        // Columns
+        int winIn = 0;
+        for (int i = 0; i < cols; i++) {
+            for (int j = 0; j < cols; j++) {
+                for (int k = 0; k < rows; k++) {
+                    if (j == winIn) {
+                        wins[i][j][k] = 1;
+                    } else {
+                        wins[i][j][k] = 0;
+                    }
+                }
+            }
+            winIn++;
+        }
+        // Rows
+        winIn = 0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < rows; j++) {
+                for (int k = 0; k < cols; k++) {
+                    if (k == winIn) {
+                        wins[i + cols][j][k] = 1;
+                    } else {
+                        wins[i + cols][j][k] = 0;
+                    }
+                }
+            }
+            winIn++;
+        }
+        // Diagonal - upper left to lower right. Assumes square board.
+        int i = cols + rows;
+        for (int j = 0; j < rows; j++) {
+            for (int k = 0; k < cols; k++) {
+                if (j == k) {
+                    wins[i][j][k] = 1;
+                } else {
+                    wins[i][j][k] = 0;
+                }
+            }
+        }
+        // Diagonal - upper right to lower left. Assumes square board.
+        i++;
+        for (int j = 0; j < rows; j++) {
+            for (int k = 0; k < cols; k++) {
+                if (j + k == rows - 1) {
+                    wins[i][j][k] = 1;
+                } else {
+                    wins[i][j][k] = 0;
+                }
+            }
+        }
+        System.out.println();
     }
 }
