c2b8ca30079491a8c1c893504583af440dac0836
Douglas Urner <durner@upsd.wednet.edu>
2016-12-03 11:39:19 -0800

    Detect end of game and show winning pattern (at least on Windows).

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,4 @@
 import javax.swing.*;
-import javax.swing.plaf.DimensionUIResource;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,7 +13,7 @@ public class Board {
 
     private static JButton spaces[];
     private static int turn = 0;
-    //private static int[][][] wins;
+    private static Color background = new Color(255, 255, 255);
 
     Board(String title, int width, int height) {
         JFrame jf = new JFrame(title);
@@ -32,17 +31,13 @@ public class Board {
 
         jf.setSize(width * 100, height * 100);
         jf.setVisible(true);
-
-        //initWins(width, height);
     }
 
     private static class TTTButton extends JButton implements ActionListener {
 
-        boolean win = false;    // Not yet.
-
         public TTTButton() {
             super();
-            setBackground(Color.ORANGE);
+            setBackground(background);
             setOpaque(true);
             //setBorderPainted(false);
             //setBorder(null);
@@ -62,26 +57,32 @@ public class Board {
                 // XXX - Do something sensible.
             } else {
                 turn++;
+                if (turn >= spaces.length) {
+                    //  Game over, played to draw.
+                    playAgain();
+                }
             }
         }
     }
 
+    private static int[][] wins = new int[][]{
+            {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // horizontal wins
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // vertical wins
+            {0, 4, 8}, {2, 4, 6}             // diagonal wins
+    };
+
     private static boolean checkForWin() {
 
-        int[][] wins = new int[][] {
-                {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // horizontal wins
-                {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // vertical wins
-                {0, 4, 8}, {2, 4, 6}			 // diagonal wins
-        };
         boolean haveWin = false;
 
         for (int i = 0; i < wins.length; i++) {
-            if ( !spaces[wins[i][0]].getText().equals(" ")
-                    && spaces[wins[i][0]].getText().equals( spaces[wins[i][1]].getText() )
-                    && spaces[wins[i][1]].getText().equals( spaces[wins[i][2]].getText() )
+            if (!spaces[wins[i][0]].getText().equals(" ")
+                    && spaces[wins[i][0]].getText().equals(spaces[wins[i][1]].getText())
+                    && spaces[wins[i][1]].getText().equals(spaces[wins[i][2]].getText())
                     ) {
                 haveWin = true;
                 showWin(i);
+                playAgain();
             }
         }
         return haveWin;
@@ -89,6 +90,12 @@ public class Board {
 
     private static void showWin(int win) {
         System.out.println("Winner: pattern " + win);
+        for (int i : wins[win]) {
+            spaces[i].setBackground(Color.RED);
+        }
+    }
+
+    private static void playAgain() {
         int newGame = JOptionPane.showConfirmDialog(null, "Play again?", "Game Over", JOptionPane.YES_NO_OPTION);
         if (newGame == JOptionPane.YES_OPTION) {
             resetGame();
@@ -100,67 +107,8 @@ public class Board {
     public static void resetGame() {
         for (JButton s : spaces) {
             s.setText(" ");
+            s.setBackground(background);
         }
         turn = 0;
     }
-
-    /*private static void initWins(int cols, int rows) {
-        *//*
-         * Winning patterns are horizontal rows, vertical columns, and the two diagonals.
-         *//*
-        int patterns = cols + rows + 2;
-        //wins = new int[patterns][cols][rows];
-        // Columns
-        int winIn = 0;
-        for (int i = 0; i < cols; i++) {
-            for (int j = 0; j < cols; j++) {
-                for (int k = 0; k < rows; k++) {
-                    if (j == winIn) {
-                        wins[i][j][k] = 1;
-                    } else {
-                        wins[i][j][k] = 0;
-                    }
-                }
-            }
-            winIn++;
-        }
-        // Rows
-        winIn = 0;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < rows; j++) {
-                for (int k = 0; k < cols; k++) {
-                    if (k == winIn) {
-                        wins[i + cols][j][k] = 1;
-                    } else {
-                        wins[i + cols][j][k] = 0;
-                    }
-                }
-            }
-            winIn++;
-        }
-        // Diagonal - upper left to lower right. Assumes square board.
-        int i = cols + rows;
-        for (int j = 0; j < rows; j++) {
-            for (int k = 0; k < cols; k++) {
-                if (j == k) {
-                    wins[i][j][k] = 1;
-                } else {
-                    wins[i][j][k] = 0;
-                }
-            }
-        }
-        // Diagonal - upper right to lower left. Assumes square board.
-        i++;
-        for (int j = 0; j < rows; j++) {
-            for (int k = 0; k < cols; k++) {
-                if (j + k == rows - 1) {
-                    wins[i][j][k] = 1;
-                } else {
-                    wins[i][j][k] = 0;
-                }
-            }
-        }
-        System.out.println();
-    }
-    */
 }
